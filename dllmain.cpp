311aaa56bb0b018c95f40b166e8bd17df97e082e
humancarpet18 <epicgamirr133769+alt4@gmail.com>
2024-07-06 17:43:12 -0700

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -4,7 +4,7 @@
 void printsploit()
 {
       typedef int(__cdecl* print_t)(int, const char*, ...);
-	    print_t r_print = (print_t)rebase(0x0172CFD0); // print address for version-1088f3c8e4a44cc7 7/5/2024
+      print_t r_print = (print_t)rebase(0x0172CFD0); // print address for version-1088f3c8e4a44cc7 7/5/2024
       while(true)
       {
             r_print(0, "Hello from printsploit!");
